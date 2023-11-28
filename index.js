775f95dda2c6cacfe0205d8b470a9604f1703e9e
Kevin Soares <117417987+kevinssauro@users.noreply.github.com>
2023-11-28 12:43:49 -0300

    Desáfio finalizado (JS e HTML)

--- index.js
+++ index.js
@@ -27,7 +27,26 @@ Ao final deve se exibir uma mensagem:
 "O Herói de nome **{nome}** está no nível de **{nivel}**"
 */
 
-let nome = "kevin"
-let xp = 0
+let nome = prompt("Digite seu nome")
+let xp = prompt("Digite seu nível (de 0 a 10000)")
 let nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
 
+if ((xp <= 1000)){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[0])
+}else if(xp >= 1001 && xp <= 2000){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[1])
+}else if(xp >= 1001 && xp <= 5000){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[2])
+}else if(xp >= 6001 && xp <= 7000){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[3])
+}else if(xp >= 7001 && xp <= 8000){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[4])
+}else if(xp >= 8001 && xp <= 9000){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[5])
+}else if(xp >= 9001 && xp <=10000){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[6])
+}else if(xp >= 10001){
+    console.log("O herói chamado " + nome + " está no nível: " + nivel[7])
+}else{
+    console.log("Digite um valor válido")
+}
